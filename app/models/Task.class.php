c80743fdf137635c5c0b6d6417d20787b24e5aed
ulricaskarin <ulrica.skarin@hotmail.se>
2021-03-26 16:32:39 +0100

    Add Task Class layout

---
+++ app/models/Task.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ★ Task Class ★
+ *
+ * Extends Model Class.
+ *
+ * Every call to the database concerning [tasks] table
+ * goes through the Task Class.
+ *
+ * @author: ulricaskarin
+ * @version 1.0.0
+ */
+
+ namespace models;
+
+ class Task extends \models\Model
+ {
+   
+ }
