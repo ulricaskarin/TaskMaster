e5373fce935cbb3eb94eb45c9b662ea1b963e0a1
ulricaskarin <ulrica.skarin@hotmail.se>
2021-03-27 08:12:22 +0100

    Add skeleton for Task controller

---
+++ app/controllers/TaskController.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ★ Task Controller ★
+ *
+ * Serves the view with data from model
+ * Serves model with data from view
+ *
+ * @author: ulricaskarin
+ * @version 1.0.0
+ */
+
+namespace controllers;
+
+class TaskController
+{
+
+  public function __construct()
+  {
+     
+  }
+}
