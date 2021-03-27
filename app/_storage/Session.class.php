84ebd0597f1695554ef28e15051a49e67630ea99
ulricaskarin <ulrica.skarin@hotmail.se>
2021-03-27 19:00:15 +0100

    Add Set and Get Session

--- app/_storage/Session.class.php
+++ app/_storage/Session.class.php
@@ -14,31 +14,57 @@ namespace _storage;
  */
 class Session
 {
-    // Static session variables for different sessions:
-    public static $flashMessage = 'flashMessage';
-    public static $taskAddedSuccess = 'taskAddedSucces';
+  // Static session variables for different sessions:
+  public static $flashMessage = 'flashMessage';
+  public static $taskAddedSuccess = 'taskAddedSucces';
 
-    const ZERO = 0;
+  const ZERO = 0;
 
-    /**
-     * Starts session.
-     */
-    public static function startSession()
-    {
-        if (session_status() === PHP_SESSION_NONE) {
-            self::setIniSessionSettings();
-            self::setSessionCookieParams();
-            session_start();
-        }
+  /**
+   * Starts session.
+   */
+  public static function startSession()
+  {
+    if (session_status() === PHP_SESSION_NONE) {
+      self::setIniSessionSettings();
+      self::setSessionCookieParams();
+      session_start();
     }
+  }
 
-    /**
-     * Sets session configuration in ini.
-     * Settings hinder the passing of session id in URL.
-     */
-    private static function setIniSessionSettings()
-    {
-        ini_set('session.use_only_cookies', true);
-        ini_set('session.use_trans_sid', false);
+  /**
+   * Sets session configuration in ini.
+   * Settings hinder the passing of session id in URL.
+   */
+  private static function setIniSessionSettings()
+  {
+    ini_set('session.use_only_cookies', true);
+    ini_set('session.use_trans_sid', false);
+  }
+
+  /**
+   * Sets a session.
+   *
+   * @param string $key
+   * @param string $value
+   */
+  public static function set(string $key, string $value)
+  {
+    $_SESSION[$key] = $value;
+  }
+
+  /**
+   * Gets a session.
+   *
+   * @param string $key
+   * @return bool, true if session is set
+   */
+  public static function get(string $key) : bool
+  {
+    if (isset($_SESSION[$key])) {
+      return $_SESSION[$key];
+    } else {
+      return false;
     }
+  }
 }
