f0feb3e680bd23724bba3ab261f1ff24dc554bec
ulricaskarin <ulrica.skarin@hotmail.se>
2021-03-28 17:31:08 +0200

    Add Message View (partial)

---
+++ app/views/MessageView.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ★ MessageView ★
+ *
+ * @author: ulricaskarin
+ * @version 1.0.0
+ */
+
+namespace views;
+
+/**
+ * Class MessageView.
+ *
+ * Static messages sent to user.
+ *
+ * @package view
+ */
+class MessageView
+{
+    public static $addTaskSuccess = "Task is successfully added! Add more?";
+}
