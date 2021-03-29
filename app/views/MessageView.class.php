caff01251b9a81de02f4790396ff6b1c5432a484
ulricaskarin <ulrica.skarin@hotmail.se>
2021-03-29 11:53:25 +0200

    Add response messages to priority GET requests

--- app/views/MessageView.class.php
+++ app/views/MessageView.class.php
@@ -17,5 +17,8 @@ namespace views;
  */
 class MessageView
 {
-    public static $addTaskSuccess = "Task is successfully added! Add more?";
+  public static $addTaskSuccess = 'Task is successfully added! Add another one?';
+  public static $priority_1 = 'These are your high priority tasks! Go finish them!';
+  public static $priority_2 = 'These are your low priority tasks! Not in a hurry to finish huh?!';
+  public static $nothingReturned = 'Your request returned empty. Want to add a task?';
 }
